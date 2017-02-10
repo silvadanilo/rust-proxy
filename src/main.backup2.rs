edc7f87dd0359dc51e013a7bacc763ab6ce1a4b6
SAMPLE <SAMPLE>
2017-02-10 18:48:16 +0100

    share code

--- src/main.backup2.rs
+++ src/main.backup2.rs
@@ -10,7 +10,7 @@ use log::LogLevel;
 use futures::sync::mpsc;
 use futures::{finished, Future, Stream, Sink, stream};
 use std::{io, str};
-use tokio_core::io::{Io, Codec, EasyBuf, Framed};
+use tokio_core::io::{Io, Codec, EasyBuf};
 use tokio_core::net::{TcpStream, TcpListener};
 use tokio_core::reactor::{Core, Handle};
 use tokio_line::LineCodec;
@@ -22,114 +22,86 @@ use futures::Async::{NotReady, Ready};
 use futures::Async;
 use std::borrow::Borrow;
 use futures::sync::mpsc::UnboundedReceiver;
-use std::net::SocketAddr;
-use std::collections::HashMap;
 
-#[derive(Clone)]
-struct Client {
-    tx: mpsc::Sender<String>,
-}
+// struct Client {
+//     rx: Stream<Item=String, Error=io::Error>,
+// }
 
-impl Client {
-    fn new(tx: mpsc::Sender<String>) -> Client {
-        Client {
-            tx: tx,
-        }
-    }
-}
+// impl Client {
+//     // pub fn get_connection<S>(&self, handle: &Handle) -> Box<Future<Item = (), Error = io::Error>>
+//     pub fn get_connection<S>(&self, handle: &Handle) -> Box<Future<Item = (), Error = io::Error>>
+//     {
+//         let remote_addr = "127.0.0.1:9876".parse().unwrap();
+//         let tcp = TcpStream::connect(&remote_addr, handle);
+//         let handle_clone = handle.clone();
+//         // let rx_rc_clone = rx_rc.clone();
 
-#[derive(Clone)]
-struct ConnectedClients(Rc<RefCell<HashMap<u32, Client>>>);
+//         // let client = tcp.and_then(|stream| -> Result<(), io::Error> {
+//         // let client = tcp.and_then(|stream| -> Result<futures::Async<u8>, io::Error> {
+//         let client = tcp.and_then(|stream| {
+//             let (sink, from_server) = stream.framed(LineCodec).split();
+//             // let reader = from_server.for_each(|message| {
+//             //     println!("{}", message);
+//             //     Ok(())
+//             // });
 
-impl ConnectedClients {
-    pub fn new() -> ConnectedClients {
-        ConnectedClients(Rc::new(RefCell::new(HashMap::new())))
-    }
+//             self.rx.forward(sink)
+//                 .map_err(|_| io::Error::new(io::ErrorKind::Other, "fail to forward"))
+//                 .map(|_| ())
 
-    pub fn insert(&self, i: u32, client: Client) {
-        self.0.borrow_mut().insert(i, client);
-    }
+//                 // let writer = rx.forward(sink)
+//                 //     .map_err(|_| io::Error::new(io::ErrorKind::Other, "fail to forward"))
+//                 //     .map(|_| ());
+//                 // writer
 
-    pub fn remove(&self, i: &u32) -> Option<Client> {
-        self.0.borrow_mut().remove(i)
-    }
+//                 // reader.select(writer).and_then(move |_| {
+//                 //     println!("CLIENT DISCONNECTED");
+//                 //     // Attempt to reconnect in the future
+//                 //     // get_connection(&handle_clone, rx)
+//                 //     Err(io::Error::new(io::ErrorKind::Other, "clinet disconnected"))
+//                 // })
+//         });
 
-    pub fn broadcast<E: 'static>(&self, message: String) -> Box<Future<Item = (), Error = E>> {
-        let client_map: std::cell::RefMut<HashMap<u32, Client>> = self.0.borrow_mut();
+//         // let client = client.and_then(|stream| {
+//         // });
 
-        // For each client, clone its `mpsc::Sender` (because sending consumes the sender) and
-        // start sending a clone of `message`. This produces an iterator of Futures.
-        let clients = client_map.values();
-        let all_sends = clients.map(|client| client.tx.clone().send(message.clone()));
-
-        // Collect the futures into a stream. We don't care about:
-        //
-        //    1. what order they finish (hence `futures_unordered`)
-        //    2. the result of any individual send (hence the `.then(|_| Ok(()))`. If the send
-        //       succeeds we don't need the `Sender` back since we still have it in our hashmap.
-        //       If the send fails its because the receiver is gone, so we don't need to broadcast
-        //       to them anyway.
-        let send_stream = stream::futures_unordered(all_sends).then(|_| Ok(()));
-
-        // Convert the stream to a future that runs all the sends and box it up.
-        Box::new(send_stream.for_each(|()| Ok(())))
-    }
-}
+//         let handle_clone = handle.clone();
+//         let client = client.or_else(move |err| {
+//             // Note: this code will infinitely retry, but you could pattern match on the error
+//             // to retry only on certain kinds of error
+//             println!("Error connecting to server: {}", err);
+//             // get_connection(&handle_clone, rx_rc)
+//             Ok(())
+//         });
+//         Box::new(client)
+//     }
+// }
 
 // // Try to avoid to return a Box because it have a runtime cost
 // // https://github.com/alexcrichton/futures-rs/blob/6a1950e6cd91cb7fd4f27b19d0090f81dc957a05/TUTORIAL.md#returning-futures
 // fn get_connection<S>(handle: &Handle, rx: S) -> Box<Future<Item = (), Error = io::Error>>
 //         where S: 'static + Stream<Item=String, Error=std::io::Error>
-// fn get_connection<S>(handle: &Handle, rx: S) -> Box<Future<Item = (), Error = io::Error>>
-//         where S: 'static + Stream<Item=String, Error=std::io::Error>
-// fn get_connection(handle: &Handle, c: ConnectedClients) -> Box<Future<Item = (), Error = io::Error>>
-fn get_connection(handle: &Handle, connToServer: ConnectedClients) -> Box<Future<Item = (), Error = io::Error>>
+fn get_connection<S>(handle: &Handle, rx: S) -> Box<Future<Item = (), Error = io::Error>>
+        where S: 'static + Stream<Item=String, Error=std::io::Error>
 {
     let remote_addr = "127.0.0.1:9876".parse().unwrap();
     let tcp = TcpStream::connect(&remote_addr, handle);
     let handle_clone = handle.clone();
-    let i: u32 = 0;
 
     // let client = tcp.and_then(|stream| -> Result<(), io::Error> {
     // let client = tcp.and_then(|stream| -> Result<futures::Async<u8>, io::Error> {
-    let connToServer_clone = connToServer.clone();
-    let client = tcp.and_then(move |stream| {
-        let (tx, rx) = mpsc::channel(8);
-        connToServer_clone.insert(i, Client::new(tx));
-
-        let (sender, receiver) = stream.framed(LineCodec).split();
-
-        let reader = receiver.for_each(|message| {
+    let client = tcp.and_then(|stream| {
+        let (sink, from_server) = stream.framed(LineCodec).split();
+        let reader = from_server.for_each(|message| {
             println!("{}", message);
             Ok(())
         });
 
-        let writer = rx.forward(sender)
-            // .map(|_| ())
-            .map_err(|_| io::Error::new(io::ErrorKind::Other, "fail to forward"));
+        let reader = reader.and_then(|_| {
+            Err(io::Error::new(io::ErrorKind::Other, "clinet disconnected"))
+        });
 
-        // let writer = rx
-        //     .map_err(|()| unreachable!("rx can't fail"))
-        //     .fold(sender, |sender, msg| {
-        //         sender.send(msg)
-        //         //Err(io::Error::new(io::ErrorKind::Other, "aa"))
-        //     })
-        //     .map(|_| ());
-        //     // .map_err(|()| Err(io::Error::new(io::ErrorKind::Other, "aaargh")))
-        //     // .map(|_| ());
-
-        reader.and_then(move |_| {
-            println!("Client Disconnected");
-            Err(io::Error::new(io::ErrorKind::Other, "client disconnected"))
-        })
-        // reader.select(writer).map(move |_| {
-        //     println!("Client Disconnected");
-        //     Err(io::Error::new(io::ErrorKind::Other, "client disconnected"))
-        // }).map_err(|(err, _)| {
-        //     ()
-        //     // println!("Errore nel reader: {}", err);
-        //     // err
-        // })
+        reader
 
         // Ok(())
         // rx.forward(sink)
@@ -142,7 +114,7 @@ fn get_connection(handle: &Handle, connToServer: ConnectedClients) -> Box<Future
         // reader.select(writer).and_then(move |_| {
         //     println!("CLIENT DISCONNECTED");
         //     // Attempt to reconnect in the future
-        //     // get_connection(&handle_clone, connToServer_clone)
+        //     // get_connection(&handle_clone, rx)
         //     Err(io::Error::new(io::ErrorKind::Other, "clinet disconnected"))
         // })
     });
@@ -151,14 +123,12 @@ fn get_connection(handle: &Handle, connToServer: ConnectedClients) -> Box<Future
     // });
 
     let handle_clone = handle.clone();
-    let connToServer_clone = connToServer.clone();
     let client = client.or_else(move |err| {
         // Note: this code will infinitely retry, but you could pattern match on the error
         // to retry only on certain kinds of error
         println!("Error connecting to server: {}", err);
-        get_connection(&handle_clone, connToServer_clone)
+        get_connection(&handle_clone, rx)
     });
-
     Box::new(client)
 }
 
@@ -171,19 +141,11 @@ fn main() {
     let connections = listener.incoming();
     let handle = core.handle();
 
-    let clients = ConnectedClients::new();
-
     let (tx, rx) = mpsc::unbounded();
-    // let rx = rx.map_err(|_| panic!()); // errors not possible on rx
-
-    let clients_clone = clients.clone();
-    let sync = rx.for_each(move |msg| {
-        clients_clone.broadcast(msg)
-    });
-    handle.spawn(sync);
-
-
-    let client = get_connection(&handle, clients);
+    let rx = rx.map_err(|_| panic!()); // errors not possible on rx
+    // let rx_rc = Rc::new(rx);
+    // let client = get_connection(&handle, rx_rc);
+    let client = get_connection(&handle, rx);
 
     let client = client.map_err(|_| panic!()); // errors not possible on rx
     handle.spawn(client);
